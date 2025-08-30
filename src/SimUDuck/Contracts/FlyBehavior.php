fe98a51416c6989f438b1fcad52665ead3b0a9a4
DarkLionRRR <lllevaa28@gmail.com>
2025-08-30 20:13:00 +0300

    HFB-3 enable strict_types where it has not been done

--- src/SimUDuck/Contracts/FlyBehavior.php
+++ src/SimUDuck/Contracts/FlyBehavior.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace HeadFirstDesignPatterns\SimUDuck\Contracts;
 
 interface FlyBehavior
--- src/SimUDuck/Quacks/Squeak.php
+++ src/SimUDuck/Quacks/Squeak.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace HeadFirstDesignPatterns\SimUDuck\Quacks;
 
 use HeadFirstDesignPatterns\SimUDuck\Contracts\QuackBehavior;
